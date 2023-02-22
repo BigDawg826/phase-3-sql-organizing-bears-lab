bbdbeed26c7f600253ec3de0a2df08ee69a97cc3
Darian Fultz <dfultz826@yahoo.com>
2023-02-22 01:53:02 -0500

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20 , 'M', 'dark brown', 'funny', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 10 , 'M', 'black', 'playful', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 30 , 'F', 'light brown', 'docile', 0);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 15 , 'M', 'brown', 'assertive', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 5 , 'F', 'brown', 'shy', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 25 , 'M', 'brown', 'mean', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 15 , 'F', 'black', 'friendly', 1);
+INSERT INTO bears VALUES (8, null, 18 , 'M', 'brown', 'quiet', 1);
